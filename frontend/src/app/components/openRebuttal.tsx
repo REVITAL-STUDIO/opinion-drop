511250174c0ed319318baadf02cef6f12476b532
Royce Williams <roycewilliamsj@gmail.com>
2024-07-10 16:31:45 -0700

    Modals are Stationary / Rebuttals Are fixed

--- frontend/src/app/components/openRebuttal.tsx
+++ frontend/src/app/components/openRebuttal.tsx
@@ -1,14 +1,20 @@
-import React, { useState, useRef, useEffect } from "react";
+import Image from "next/image";
+import { useState, useRef, useEffect } from "react";
 import { IoIosArrowDropdown, IoIosSend } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+import RebuttalShort from "./RebuttalShort";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaPaperPlane, FaFlag, FaX } from "react-icons/fa6";
+import ProgressBar from "progressbar.js";
+import InteractionModal from "./InteractionModal";
+
+import SurveyPrompt from "./SurveyPrompt";
 import {
   faFlag,
   faPaperPlane,
   faThumbsUp,
 } from "@fortawesome/free-regular-svg-icons";
 import { faX } from "@fortawesome/free-solid-svg-icons";
-import SurveyPrompt from "./SurveyPrompt";
-import InteractionModal from "./InteractionModal";
 
 interface Highlight {
   id: string;
@@ -140,81 +146,81 @@ const OpenRebuttal: React.FC<OpenRebuttalProps> = ({
     return container;
   };
 
-  const handleTextSelect = () => {
-    if (!highlightEnabled) return;
-    const opinionText = document.querySelector(".opinion-text");
-    const selection = window.getSelection();
-    const selectedText = selection?.toString();
-    if (selection && !opinionText?.contains(selection.anchorNode)) return;
-    if (selectedText) {
-      setShowInteractionModal(true);
-      setHighlightedText(selectedText);
-      const range = selection?.getRangeAt(0);
-      const container = document.createElement("span");
-      container.style.position = "relative";
-      container.style.display = "inline-block";
-      container.style.textIndent = "0";
+  // const handleTextSelect = () => {
+  //   if (!highlightEnabled) return;
+  //   const opinionText = document.querySelector(".opinion-text");
+  //   const selection = window.getSelection();
+  //   const selectedText = selection?.toString();
+  //   if (selection && !opinionText?.contains(selection.anchorNode)) return;
+  //   if (selectedText) {
+  //     setShowInteractionModal(true);
+  //     setHighlightedText(selectedText);
+  //     const range = selection?.getRangeAt(0);
+  //     const container = document.createElement("span");
+  //     container.style.position = "relative";
+  //     container.style.display = "inline-block";
+  //     container.style.textIndent = "0";
+  
+  //     const highlightedText = document.createElement("span");
+  //     highlightedText.className = "highlightedText";
+  //     highlightedText.textContent = selectedText;
+  //     highlightedText.style.fontSize = "1.2em";
+  
+  //     const closeButton = document.createElement("button");
+  //     closeButton.className = "highlight-close-button";
+  //     closeButton.textContent = "✕";
+  //     closeButton.addEventListener("click", (event) => {
+  //       event.stopPropagation();
+  //       setShowInteractionModal(false);
+  //       setHighlightContainer(null);
+  //       const parent = container.parentNode;
+  //       if (parent) {
+  //         parent.replaceChild(document.createTextNode(selectedText), container);
+  //       }
+  //     });
+  
+  //     const editButton = document.createElement("button");
+  //     editButton.className = "highlight-edit-button";
+  //     editButton.innerHTML = `
+  //       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+  //         <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"/>
+  //       </svg>
+  //     `;
+  //     editButton.addEventListener("click", (event) => {
+  //       event.stopPropagation();
+  //       setCurrentHighlightId(highlightId);
+  //       setShowInteractionModal(true);
+  //     });
+  
+  //     container.appendChild(highlightedText);
+  //     container.appendChild(closeButton);
+  //     container.appendChild(editButton);
+  //     setHighlightContainer(container);
+  
+  //     range?.deleteContents();
+  //     range?.insertNode(container);
+  
+  //     range?.setStartAfter(container);
+  //     selection?.removeAllRanges();
+  //     if (range) {
+  //       selection?.addRange(range);
+  //     }
+  
+  //     const highlightId = generateHighlightId();
+  //     setCurrentHighlightId(highlightId);
+  //     setHighlights((prev) => [
+  //       ...prev,
+  //       { id: highlightId, container, text: selectedText },
+  //     ]);
+  //   }
+  // };
 
-      const highlightedText = document.createElement("span");
-      highlightedText.className = "highlightedText";
-      highlightedText.textContent = selectedText;
-      highlightedText.style.fontSize = "1.2em";
-
-      const closeButton = document.createElement("button");
-      closeButton.className = "highlight-close-button";
-      closeButton.textContent = "✕";
-      closeButton.addEventListener("click", (event) => {
-        event.stopPropagation();
-        setShowInteractionModal(false);
-        setHighlightContainer(null);
-        const parent = container.parentNode;
-        if (parent) {
-          parent.replaceChild(document.createTextNode(selectedText), container);
-        }
-      });
-
-      const editButton = document.createElement("button");
-      editButton.className = "highlight-edit-button";
-      editButton.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-          <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"/>
-        </svg>
-      `;
-      editButton.addEventListener("click", (event) => {
-        event.stopPropagation();
-        setCurrentHighlightId(highlightId);
-        setShowInteractionModal(true);
-      });
-
-      container.appendChild(highlightedText);
-      container.appendChild(closeButton);
-      container.appendChild(editButton);
-      setHighlightContainer(container);
-
-      range?.deleteContents();
-      range?.insertNode(container);
-
-      range?.setStartAfter(container);
-      selection?.removeAllRanges();
-      if (range) {
-        selection?.addRange(range);
-      }
-
-      const highlightId = generateHighlightId();
-      setCurrentHighlightId(highlightId);
-      setHighlights((prev) => [
-        ...prev,
-        { id: highlightId, container, text: selectedText },
-      ]);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mouseup", handleTextSelect);
-    return () => {
-      document.removeEventListener("mouseup", handleTextSelect);
-    };
-  }, [highlightEnabled]);
+  // useEffect(() => {
+  //   document.addEventListener("mouseup", handleTextSelect);
+  //   return () => {
+  //     document.removeEventListener("mouseup", handleTextSelect);
+  //   };
+  // }, [highlightEnabled]);
 
   
 
@@ -299,7 +305,7 @@ const OpenRebuttal: React.FC<OpenRebuttalProps> = ({
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum. <br></br>
-            <br></br>"Sed ut perspiciatis unde omnis iste natus error sit
+            <br></br>Sed ut perspiciatis unde omnis iste natus error sit
             voluptatem accusantium doloremque laudantium, totam rem aperiam,
             eaque ipsa quae ab illo inventore veritatis et quasi architecto
             beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
@@ -312,8 +318,8 @@ const OpenRebuttal: React.FC<OpenRebuttalProps> = ({
             laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
             vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
             molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-            nulla pariatur?"<br></br>
-            <br></br>"Sed ut perspiciatis unde omnis iste natus error sit
+            nulla pariatur?<br></br>
+            <br></br>Sed ut perspiciatis unde omnis iste natus error sit
             voluptatem accusantium doloremque laudantium, totam rem aperiam,
             eaque ipsa quae ab illo inventore veritatis et quasi architecto
             beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
@@ -326,7 +332,7 @@ const OpenRebuttal: React.FC<OpenRebuttalProps> = ({
             laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
             vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
             molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-            nulla pariatur?"
+            nulla pariatur?
           </p>
         </div>
       </div>
