dd202c86d86e5061a69cacd4de0b2ceda790fc10
roycejwilliams <roycewilliamsj@gmail.com>
2024-07-16 22:02:28 -0700

    update

--- frontend/src/app/components/adminPortal.tsx
+++ frontend/src/app/components/adminPortal.tsx
@@ -2,9 +2,11 @@
 
 import {
   faArrowRightFromBracket,
+  faArrowRightLong,
   faGears,
   faHeart,
   faPenFancy,
+  faSquareH,
   faUser,
   faUsers,
 } from "@fortawesome/free-solid-svg-icons";
@@ -65,27 +67,25 @@ const AdminPortal = () => {
       `,
     },
   ];
+  // bg-[#f6f6f6]
 
   return (
-    <section className="bg-gradient-to-b from-black via-blue-300 to-red-300 w-full h-screen flex">
+    <section className="bg-gradient-to-t via-blue-400 from-red-300 to-gray-200 w-full h-screen flex">
       <div className="w-1/6 h-full ">
-        <div className="p-4 flex justify-center items-center">
+        <div className="p-4 flex  items-center mt-[15%]">
           <Image
             src="/Images/opinion-drop-logo.png"
             alt="opinion drop logo"
-            width={150}
-            height={150}
-            className="mx-auto "
+            width={100}
+            height={100}
+            className="px-4"
           />
         </div>
-        <div className="flex flex-col  text-white gap-y-4 p-4">
+        <div className="flex flex-col  text-black gap-y-4 p-4">
           <button className="p-4 w-fit text-base text-left font-semibold hover:bg-purple-600 hover:text-white duration-150 ease-in-out transition rounded-3xl flex items-center gap-x-4">
-            <FontAwesomeIcon icon={faPenFancy} className="" /> Essays
+            <FontAwesomeIcon icon={faSquareH} /> Home
           </button>
-          <button className="p-4 w-fit text-base text-left font-semibold hover:bg-purple-600 hover:text-white duration-150 ease-in-out transition rounded-3xl flex items-center gap-x-4">
-            <FontAwesomeIcon icon={faHeart} className="" />
-            Liked
-          </button>
+
           <button className="p-4 w-fit text-base text-left font-semibold hover:bg-purple-600 hover:text-white duration-150 ease-in-out transition rounded-3xl flex items-center gap-x-4">
             <FontAwesomeIcon icon={faGears} className="" />
             Settings
@@ -97,28 +97,63 @@ const AdminPortal = () => {
       </div>{" "}
       <div className="w-5/6 h-full text-white">
         {/* UserName */}
-        <div className="p-4  mt-[4%]  rounded-full w-fit">
-          <h1 className="text-3xl text-white border-b border-b-blue-500">
-            Welcome, <span className="font-bold">Vicki</span>
+        <div className="p-4 mt-[4%] rounded-full w-fit flex items-center gap-x-4">
+          <div className="w-[4rem] h-[4rem] rounded-full shadow-md bg-white"></div>
+          <h1 className="text-3xl text-black ">
+            Welcome, <span className="font-bold">User</span>
           </h1>
         </div>
-        <div className="mt-[4%] h-1/5 w-full  text-white p-4">
-          <h2 className="text-2xl font-semibold">Essays</h2>
-          <div className="my-4  h-3/4 flex gap-4">
+        {/* Performance */}
+        <div className="p-4  my-[4%] ">
+          <h2 className="text-lg w-fit font-semibold border text-black border-black p-4 rounded-full">
+            Performance
+          </h2>
+          <div className="w-full flex gap-x-4 text-black mt-4">
+            <div className="p-4 shadow-lg rounded-xl flex flex-col bg-white">
+              <span>14k</span>
+              Total Views
+            </div>
+            <div className="p-4 shadow-lg rounded-xl flex flex-col bg-white">
+              <span>103</span>
+              Shares
+            </div>
+            <div className="p-4 shadow-lg rounded-xl flex flex-col bg-white">
+              <span>5</span> # times ranked
+            </div>
+            <div className="p-4 shadow-lg rounded-xl flex flex-col bg-white">
+              <span>345</span>
+              Total Likes
+            </div>
+          </div>
+        </div>
+        {/* Essay Submission */}
+        <div className="mt-[4%]  w-full rounded-3xl text-white p-4">
+          <h2 className="text-lg w-fit font-semibold border text-black border-black p-4 rounded-full">
+            Essays Submitted
+          </h2>
+          <div className="my-4 grid grid-cols-3 gap-2">
             {slides.map((slide, index) => (
-              <div key={index} className="py-[8%] border w-1/4 rounded-2xl relative overflow-hidden shadow-md">
+              <div
+                key={index}
+                className="p-[15%] border rounded-2xl relative overflow-hidden shadow-md"
+              >
                 <Image
                   src={slide.backgroundImage}
                   alt={slide.author}
                   fill
                   className=" w-[100%] h-[100%] object-cover object-center brightness-75"
                 />
-                <h2 className="font-semibold absolute px-4 text-lg">{slide.title}</h2>
+                <h2 className="font-semibold absolute mx-auto text-base text-center">
+                  {slide.title}
+                </h2>
               </div>
             ))}
+            <div className="p-[15%] bg-white rounded-2xl text-black hover:scale-110 transition ease-in-out duration-150 shadow-xl flex items-center gap-x-4">
+              <h2 className="text-base font-semibold">Archive</h2>
+              <FontAwesomeIcon icon={faArrowRightLong} />
+            </div>
           </div>
         </div>
-        
       </div>
     </section>
   );
